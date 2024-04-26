46ab5c4c6052823513ded77909924d38e1289326
Firoz Mahmud Rizvi <firoz.rizvi@brainstation-23.com>
2024-04-26 11:23:13 +0600

    M cpp

--- basic_program.cpp
+++ basic_program.cpp
@@ -2,5 +2,15 @@
 
 
 int main(){
-    cout<<"Hello World"
+    cout<<"Hello "; //intentionally deleted world..
 }
+
+int calculateFactorial(int n){
+    int result =1 ;
+    for(int i=1 ; i<=n ; i++){
+        result *=i;
+    }
+
+    return result;
+}
+frizvi99/Git-Assignment-Individual-1-BS1560
